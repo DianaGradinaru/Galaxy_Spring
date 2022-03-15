734af1f67fec992d9f4b9348302d0958e11250a6
DianaGradinaru <diana.gradinaru.sincai@gmail.com>
2022-03-15 09:30:01 +0200

    Web Security cors added

--- server/src/main/java/com/example/server/security/config/WebSecurityConfig.java
+++ server/src/main/java/com/example/server/security/config/WebSecurityConfig.java
@@ -10,7 +10,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
@@ -22,6 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/v*/registration/**")
